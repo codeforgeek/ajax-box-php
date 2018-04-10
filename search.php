9b06a6d70e3a5ea1c15885d75a40d9bb8b22a107
Shahid Shaikh <rwtc66@gmail.com>
2018-04-10 19:49:24 +0530

    Update search.php

--- search.php
+++ search.php
@@ -1,12 +1,12 @@
 <?php
     $key=$_GET['key'];
     $array = array();
-    $con=mysql_connect("localhost","root","");
-    $db=mysql_select_db("demos",$con);
-    $query=mysql_query("select * from cfg_demos where title LIKE '%{$key}%'");
-    while($row=mysql_fetch_assoc($query))
+    $con=mysqli_connect("localhost","root","","demos");
+    $query=mysqli_query($con, "select * from cfg_demos where title LIKE '%{$key}%'");
+    while($row=mysqli_fetch_assoc($query))
     {
       $array[] = $row['title'];
     }
     echo json_encode($array);
+    mysqli_close($con);
 ?>
